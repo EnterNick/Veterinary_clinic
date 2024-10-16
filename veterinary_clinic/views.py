4793ad555d257349a56267e19ba5c5cc0dcf658c
EnterNick <151045256+EnterNick@users.noreply.github.com>
2024-10-16 23:47:37 +0300

    Update views.py

--- veterinary_clinic/views.py
+++ veterinary_clinic/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+
 from .models import Services, Person, Settings
 from django.contrib.auth.models import Group
 from rest_framework import permissions, viewsets
 from veterinary_clinic.serializes import GroupSerializer, UserSerializer, IndexSerializer
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.views import APIView
 
 
-def index(request):
-    servises_list = Services.objects.order_by("-id")[:5]
-    context = {
-        "servises": servises_list,
-        'settings': Settings.objects.first()
-    }
-    return render(request, "veterinary_clinic/index.html", context)
+# def index(request):
+#     servises_list = Services.objects.order_by("-id")[:5]
+#     context = {
+#         "servises": servises_list,
+#         'settings': Settings.objects.first()
+#     }
+#     return render(request, "veterinary_clinic/index.html", context)
+
+
+class IndexView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'veterinary_clinic/index.html'
+
+    def get(self, request):
+        servises = Services.objects.all()[:5]
+        settings = Settings.objects.first()
+        return Response({'servisces': servises, 'settings': settings})
 
 
 class UserViewSet(viewsets.ModelViewSet):
