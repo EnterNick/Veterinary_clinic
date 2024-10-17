44e95b10c72aca98e4760ca8a4840377aff7f47f
EnterNick <151045256+EnterNick@users.noreply.github.com>
2024-10-17 15:10:07 +0300

    Update views.py

--- veterinary_clinic/views.py
+++ veterinary_clinic/views.py
@@ -1,21 +1,11 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 
 from .models import Services, Person, Settings
 from django.contrib.auth.models import Group
 from rest_framework import permissions, viewsets
-from veterinary_clinic.serializes import GroupSerializer, UserSerializer, IndexSerializer
+from veterinary_clinic.serializes import GroupSerializer, UserSerializer, MakeRequestSerializer
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.views import APIView
-
-
-# def index(request):
-#     servises_list = Services.objects.order_by("-id")[:5]
-#     context = {
-#         "servises": servises_list,
-#         'settings': Settings.objects.first()
-#     }
-#     return render(request, "veterinary_clinic/index.html", context)
 
 
 class IndexView(APIView):
@@ -23,9 +13,9 @@ class IndexView(APIView):
     template_name = 'veterinary_clinic/index.html'
 
     def get(self, request):
-        servises = Services.objects.all()[:5]
+        services = Services.objects.all()[:5]
         settings = Settings.objects.first()
-        return Response({'servisces': servises, 'settings': settings})
+        return Response({'services': services, 'settings': settings})
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -38,3 +28,20 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all().order_by('name')
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class MakeRequestView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'veterinary_clinic/make_request_form.html'
+
+    def get(self, request):
+        serializer = MakeRequestSerializer()
+        serializer.name = request.user.username
+        settings = Settings.objects.first()
+        return Response({'serializer': serializer, 'settings': settings})
+
+    def post(self, request):
+        serializer = MakeRequestSerializer()
+        serializer.name = request.user.name
+        settings = Settings.objects.first()
+        return Response({'serializer': serializer, 'settings': settings})
